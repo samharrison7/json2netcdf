5d9d6e9ee23d4ff36ca0671708cad88fb2783667
Sam Harrison <samharrison.xg@gmail.com>
2018-02-08 16:16:43 +0000

    Tidying up json2netcdf-compact.json.

--- json2netcdf-compact.py
+++ json2netcdf-compact.py
@@ -46,85 +46,27 @@ def parse(json_group, nc_data, hierarchy=[], root=True):
                     parse(external_data, nc_data, hierarchy + [name], False)
                 # Otherwise, it must be a variable
                 else:
-                    # Get the dimensions from the name, which are between square brackets
-                    dimensions = re.findall('\[(.*?)\]', name)
-                    # Then retrieve just the name, without the dimensions (square brackets)
-                    parsed_name = name.split('[')[0]
-                    # Convert to numpy array to get dtype object
-                    np_data = np.array(external_data)
-                    # Create the variable
-                    nc_var = nc_data.createVariable(
-                        '/' + '/'.join(hierarchy + [parsed_name]),
-                        np_data.dtype,
-                        tuple(dimensions)
-                    )
-                    # Fill the variable
-                    nc_var[:] = np_data
+                    parse_var(name, external_data, nc_data, hierarchy)
             # Otherwise, it must be data (not from external file)
             else:
-                # Get the dimensions from the name, which are between square brackets
-                dimensions = re.findall('\[(.*?)\]', name)
-                # Then retrieve just the name, without the dimensions (square brackets)
-                parsed_name = name.split('[')[0]
-                # Convert to numpy array to get dtype object
-                np_data = np.array(data)
-                # Create the variable
-                nc_var = nc_data.createVariable(
-                    '/' + '/'.join(hierarchy + [parsed_name]),
-                    np_data.dtype,
-                    tuple(dimensions)
-                )
-                # Fill the variable
-                nc_var[:] = np_data
+                parse_var(name, data, nc_data, hierarchy)
 
-            
-
-    #     # If this is the root group, don't create new group for it.
-    #     # Any name specified will be ignored.
-    #     if root:
-    #         nc_group = nc_data
-    #     else:
-    #         hierarchy.append(json_data['name'])                             # Build the hierarchy
-    #         nc_group = nc_data.createGroup('/' + '/'.join(hierarchy))       # Create the group (with correct hierarchy)
-    #     # Does the group have dimensions?
-    #     if 'dimensions' in json_data:
-    #         for dimension in json_data['dimensions']:
-    #             # Create a dimension with either given or unlimited size
-    #             if ('size' not in dimension) or dimension['size'] == 'unlimited':
-    #                 nc_group.createDimension(dimension['name'], None)   # Unlimited
-    #             else:
-    #                 nc_group.createDimension(dimension['name'], dimension['size'])  # Specified size
-    #     # Does the group have attributes?
-    #     if 'attributes' in json_data:
-    #         for attribute in json_data['attributes']:
-    #             setattr(nc_group, attribute['name'], attribute['value'])                                                                         
-    #     # As we're in a group, recursively call this function until we reach variable
-    #     if 'data' in json_data:
-    #         for nested_data in json_data['data']:
-    #             parse(nested_data, nc_data, hierarchy, False)
-
-    # # If this is a variable, add it and its dimensions
-    # elif json_data['type'] == 'variable':
-    #     # If we're trying to get data from external file
-    #     if isinstance(json_data['data'], str) and ".json" in json_data['data']:
-    #         with open(base_dir + json_data['data']) as external_data:
-    #             external_data = json.loads(external_data.read())
-    #         json_data['data'] = external_data
-    #     # Have dimensions been specified or are we dealing with a scalar?
-    #     if 'dimensions' in json_data:
-    #         nc_var = nc_data.createVariable('/' + '/'.join(hierarchy) + '/' + json_data['name'],
-    #                                         json_data['datatype'],
-    #                                         tuple(json_data['dimensions']))
-    #     else:
-    #         nc_var = nc_data.createVariable('/' + '/'.join(hierarchy) + '/' + json_data['name'],
-    #                                         json_data['datatype'])
-    #     # Does the variable have attributes?
-    #     if 'attributes' in json_data:       
-    #         for attribute in json_data['attributes']:
-    #             setattr(nc_var, attribute['name'], attribute['value'])
-    #     # Put the data into the newly created variable
-    #     if 'data' in json_data:
-    #         nc_var[:] = json_data['data']
+# Parse a variable item, given its name, data and hierarchy
+def parse_var(name, data, nc_data, hierarchy):
+    # Get the dimensions from the name, which are between square brackets
+    dimensions = re.findall('\[(.*?)\]', name)
+    # Then retrieve just the name, without the dimensions (square brackets)
+    parsed_name = name.split('[')[0]
+    # Convert to numpy array to get dtype object
+    np_data = np.array(data)
+    # Create the variable
+    nc_var = nc_data.createVariable(
+        '/' + '/'.join(hierarchy + [parsed_name]),
+        np_data.dtype,
+        tuple(dimensions)
+    )
+    # Fill the variable
+    nc_var[:] = np_data
 
 
 # Input file
